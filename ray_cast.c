b81795e4384e1832821b403a69fbcafdf3ffca6b
Chris Duvivier <cduvivie@student.s19.be>
2020-02-17 18:04:17 +0100

    start seperation key and create image part

--- ray_cast.c
+++ ray_cast.c
@@ -6,9 +6,13 @@
 /*   By: cduvivie <cduvivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 20:18:17 by cduvivie          #+#    #+#             */
-/*   Updated: 2020/02/16 22:05:48 by cduvivie         ###   ########.fr       */
+/*   Updated: 2020/02/17 17:17:16 by cduvivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**  gcc -Wextra -Wall libmlx.a -framework OpenGl -framework AppKit -lz -L minilibx_opengl_20191021/ main.c 
+*/
 
 #include "mlx.h"
 #include <stdio.h>
@@ -29,6 +33,12 @@ typedef struct  s_vars {
     void        *mlx;
     void        *win;
 	int			done;
+	double		posX;
+	double		posY;
+	double		dirX;
+	double		planeX;
+	double		dirY;
+	double		planeY;
 }               t_vars;
 
 int worldMap[mapWidth][mapHeight]=
@@ -93,6 +103,128 @@ int		add_shade(double distance, int color)
 	return (color);
 }
 
+void	ft_draw(t_vars *vars)
+{
+	for (int x = 0; x < screenWidth; x++)
+	{
+		// calculate ray position and direction
+		double cameraX = 2 * x / screenWidth - 1; // x-coordinate in camera space
+		double rayDirX = vars->dirX + vars->planeX * cameraX;
+		double rayDirY = vars->dirY + vars->planeY * cameraX;
+	
+		// which box of the map we're in
+		int mapX = (int)(vars->posX);
+		int mapY = (int)(vars->posY);
+		
+		// length of ray from current position to next x or y-side
+		double sideDistX;
+		double sideDistY;
+		
+		// length of ray from one x or y-side to next x or y-side
+		double deltaDistX = (rayDirY == 0) ? 0 : ((rayDirX == 0) ? 1 : abs(1 / rayDirX));
+		double deltaDistY = (rayDirX == 0) ? 0 : ((rayDirY == 0) ? 1 : abs(1 / rayDirY));
+		
+		// what direction to step in x or y-direction (either +1 or -1)
+		int stepX;
+		int stepY;
+			
+		int hit = 0; // was there a wall hit?
+		int side; // was a NS or a EW wall hit?
+
+		//calculate step and initial sideDist
+		if (rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (vars->posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - vars->posX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (vars->posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - vars->posY) * deltaDistY;
+		}
+		
+		// perform DDA
+		while (hit == 0)
+		{
+			// jump to next map square, OR in x-direction, OR in y-direction
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			
+			// Check if ray has hit a wall
+			if (worldMap[mapX][mapY] > 0)
+				hit = 1;
+		}
+		// Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+		double perpWallDist;
+		if (side == 0)
+			perpWallDist = (mapX - vars->posX + (1 - stepX) / 2) / rayDirX;
+		else
+			perpWallDist = (mapY - vars->posY + (1 - stepY) / 2) / rayDirY;
+		
+		// Calculate height of line to draw on screen
+		int lineHeight = (int)(screenHeight / perpWallDist);
+
+		// calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + screenHeight / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + screenHeight / 2;
+		if (drawEnd >= screenHeight)
+			drawEnd = screenHeight - 1;
+
+		// choose wall color
+		/*
+		**	Transparency: 0xFF000000
+		**	Red: 0x00FF0000;
+		**	Green: 0x0000FF00;
+		**	Blue: 0x000000FF;
+		**
+		*/
+		int color;
+
+		switch(worldMap[mapX][mapY])
+		{
+			case 1:  color = RGB_Red;  	 break; //red
+			case 2:  color = RGB_Green;  break; //green
+			case 3:  color = RGB_Blue;   break; //blue
+			case 4:  color = RGB_White;  break; //white
+			default: color = RGB_Yellow; break; //yellow
+		}
+
+		//give x and y sides different brightness
+		if (side == 1)
+		{
+			color = color / 2;
+			// color = add_shade(0.5, color);
+		}
+
+		//draw the pixels of the stripe as a vertical line
+		// verLine(x, drawStart, drawEnd, color);
+		mlx_pixel_put(vars->mlx, vars->win, x, drawStart, color);
+		mlx_pixel_put(vars->mlx, vars->win, x, drawEnd, color);
+	} //end for loop
+}
+
 int main(int argc, char *argv[])
 {
 	t_vars      vars;
@@ -100,10 +232,6 @@ int main(int argc, char *argv[])
 	double posX = 5, posY = 5;  //x and y start position
 	double dirX = -1, dirY = 0; //initial direction vector
 	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
-	double time = 0; //time of current frame
-	double oldTime = 0; //time of previous frame
-	double w = screenWidth;
-	double h = screenHeight;
 	
 	// create screen with the resolution of choice:
 	vars.mlx = mlx_init();
@@ -112,15 +240,14 @@ int main(int argc, char *argv[])
     
 	while (!vars.done)
 	{
-		double cameraX;
-		double rayDirX;
-		double rayDirY;
-		for (int x = 0; x < w; x++)
+		// ft_draw(&vars);
+		
+		for (int x = 0; x < screenWidth; x++)
 		{
 			// calculate ray position and direction
-			cameraX = 2 * x / w - 1; // x-coordinate in camera space
-			rayDirX = dirX + planeX * cameraX;
-			rayDirY = dirY + planeY * cameraX;
+			double cameraX = 2 * x / screenWidth - 1; // x-coordinate in camera space
+			double rayDirX = dirX + planeX * cameraX;
+			double rayDirY = dirY + planeY * cameraX;
 		
 			// which box of the map we're in
 			int mapX = (int)(posX);
@@ -192,15 +319,15 @@ int main(int argc, char *argv[])
 				perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
 			
 			// Calculate height of line to draw on screen
-			int lineHeight = (int)(h / perpWallDist);
+			int lineHeight = (int)(screenHeight / perpWallDist);
 
 			// calculate lowest and highest pixel to fill in current stripe
-			int drawStart = -lineHeight / 2 + h / 2;
+			int drawStart = -lineHeight / 2 + screenHeight / 2;
 			if (drawStart < 0)
 				drawStart = 0;
-			int drawEnd = lineHeight / 2 + h / 2;
-			if (drawEnd >= h)
-				drawEnd = h - 1;
+			int drawEnd = lineHeight / 2 + screenHeight / 2;
+			if (drawEnd >= screenHeight)
+				drawEnd = screenHeight - 1;
 
 			// choose wall color
 			/*
@@ -222,13 +349,19 @@ int main(int argc, char *argv[])
 			}
 
 			//give x and y sides different brightness
-			if (side == 1) {color = color / 2;}
+			if (side == 1)
+			{
+				color = add_shade(0.5, color);
+			}
 
 			//draw the pixels of the stripe as a vertical line
 			// verLine(x, drawStart, drawEnd, color);
 			mlx_pixel_put(vars.mlx, vars.win, x, drawStart, color);
 			mlx_pixel_put(vars.mlx, vars.win, x, drawEnd, color);
-		}
+		} //end for loop
+
+		double moveSpeed = 0.5; //the constant value is in squares/second
+		double rotSpeed = 0.3; 
 		
 		mlx_loop(vars.mlx);
 	}
